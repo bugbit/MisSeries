ab535fe7acd74bce7491aa9d583c48791cec4c46
bugbit <ohernandezba@gmail.com>
2023-07-12 18:35:36 +0200

    init

---
+++ Services/TraktApi.cs
@@ -0,0 +1,20 @@
+﻿namespace MisSeries.Services;
+
+public class TraktApi
+{
+    private HttpClient _httpClient = new HttpClient { BaseAddress = new Uri("https://api.trakt.tv") };
+
+    public required string ClientId { get; set; }
+
+
+    public TraktApi()
+    {
+        _httpClient.DefaultRequestHeaders.Add("trakt-api-key", ClientId);
+        _httpClient.DefaultRequestHeaders.Add("trakt-api-version", "2");
+    }
+
+    public Task<string>GenerateNewDeviceCode()
+    {
+        _httpClient.PostAsync
+    }
+}
