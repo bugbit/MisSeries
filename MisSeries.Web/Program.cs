84fc37ca583b7ab3d953962f0ebe21bf149a6e07
bugbit <ohernandezba@gmail.com>
2023-07-14 11:41:26 +0200

    Activar multiidioma

--- MisSeries.Web/Program.cs
+++ MisSeries.Web/Program.cs
@@ -10,4 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<TraktApi>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddLocalization(opts => opts.ResourcesPath = "Resources");
+
 await builder.Build().RunAsync();
